4cfdb51d8773a6bcc85f7883e7b6e954f6bf07ec
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-15 17:51:50 +0530

    updated logic

--- yt shorts search.py
+++ yt shorts search.py
@@ -1,4 +1,6 @@
 import yt_dlp
+import datetime
+
 
 
 def filter(info_dict, *, incomplete):
@@ -16,8 +18,10 @@ def filter(info_dict, *, incomplete):
         return None 
     return 'too niche bruv remove'
 
+#elib- C:\Program Files\nodejs\node.exe
 
 
+node=r'C:\Program Files\nodejs\node.exe'
 
 
 ydl_opts= {
@@ -25,7 +29,11 @@ ydl_opts= {
     'extract_flat': False, 
     'skip_download':True,
     'match_filter': filter,
-    'javascript_runtime': 'node',
+    'javascript_runtime': node,
+    'sleep_interval': 3,
+    'max_sleep_interval': 5, 
+    'ignoreerrors': True,
+    'search_sort':'date'
     #'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
 }
 
@@ -35,13 +43,13 @@ ydl_opts= {
 
 def get_ytshorts(search):
 
-    query= f"ytsearch19:{search}"
+    query= f"ytsearch100:{search} #shorts"
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
         results = ydl.extract_info(query, download=False)
         if results is None:
-            print("ts empty bro yt blocking yo ass")
+            print("No results found.")
             return []
 
         shorts = results.get('entries', [])
@@ -49,11 +57,35 @@ def get_ytshorts(search):
         results=[]
         for i in shorts:
 
-            views= i.get('view_count')
-            sub= i.get('channel_follower_count')
+            #upload date to hours 
+            udate= i.get('upload_date')
+            if not udate:
+                continue
+
+            udatereal=datetime.datetime.strptime(udate, '%Y%m%d')
+            udaterealreal= (datetime.datetime.now() - udatereal).total_seconds()/3600
+            
+
+            duration = i.get('duration') or 0
+            views= i.get('view_count') or 0
+            sub= i.get('channel_follower_count') or 1
             ratio=views/sub
 
-            if ratio>1.5:
+            vph = views/udaterealreal
+
+    
+            print({
+                'title': i.get('title'), 
+                'id': i.get('id'),
+                'url': f"https://youtube.com/watch?v={i.get('id')}", 
+                'views': views,
+                'duration': i.get('duration'),
+                'sub': sub})
+
+            if duration == 0 or duration > 60:
+                continue
+
+            if ratio>1.5 and duration<=60 and vph > 500:
                 results.append(
                 {
                 'title': i.get('title'), 
@@ -71,10 +103,7 @@ def get_ytshorts(search):
 
 
     
-topthreeshorts=get_ytshorts(input('what you wanna look for twin?\t:'))
-
-
-
+topthreeshorts=get_ytshorts(input('what you wanna look for?\t:'))
 
 
 
